99fe146b0f66c85914d292294bf9828262490638
dnidimaneesha <96563@fhss.sjp.ac.lk>
2022-07-18 18:13:22 +0530

    update categories.js

--- frontend/src/categories.js
+++ frontend/src/categories.js
@@ -1,6 +1,6 @@
 const categories = [
     {
-        name: "Makeup items",
+        name: "Makeup Items",
         img: "https://i.pinimg.com/564x/fa/e8/6c/fae86c52a4972c5f7e7753d25bdab3e8.jpg",
     },
     {
@@ -12,7 +12,7 @@ const categories = [
 
     { name: "Makeup Tools", img: "https://i.pinimg.com/564x/d8/d8/41/d8d8419eafd7a937550ade77bdb5183e.jpg" },
 
-    { name: "nailpolish & Tools", img: "https://i.pinimg.com/564x/c3/fa/5f/c3fa5f3b8a32583695d797de71dbfe74.jpg" },
+    { name: "Nailpolish & Tools", img: "https://i.pinimg.com/564x/c3/fa/5f/c3fa5f3b8a32583695d797de71dbfe74.jpg" },
 
     { name: "Gifts", img: "https://i.pinimg.com/564x/89/be/c8/89bec87a69a984225764c0605be33602.jpg" },
 
